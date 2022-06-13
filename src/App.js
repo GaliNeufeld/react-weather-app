ef503d5036954f78dab79c784f1ffee6edc65481
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2022-06-13 16:02:14 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div class="container">
+       
       <h1>Hello from App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="mailto:galneufeld@gmail.com" target="_blank">
+        <a href="mailto:galneufeld@gmail.com" target="_blank" rel="nonreferrer"> 
           Gali Neufeld
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/GaliNeufeld/react-weather-app"
-          target="_blank"
+          target="_blank" rel="nonreferrer"
         >
           open sourced on GitHub
         </a>
       </footer>
+      </div>
     </div>
   );
   
