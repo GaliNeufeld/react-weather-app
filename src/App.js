1b026bab8ec66419f721b367e6b8a149f62302f7
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2022-06-13 14:51:53 -0400

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     return <h1>Hello from App</h1>
+      <h1>Hello from App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="mailto:galneufeld@gmail.com" target="_blank">
+          Gali Neufeld
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/GaliNeufeld/react-weather-app"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
+  
 }
 
 
