3eeec8be9dd4837a3aea0cc34ca53385479881fe
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2022-06-20 16:39:28 -0400

    Added use effect to update forecast when city is updated

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ForecastDay from "./ForecastDay";
 import "./Forecast.css";
@@ -7,6 +7,10 @@ import axios from "axios";
 export default function Forecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+    setLoaded(false);       
+    },[props.coordinates]);
     
     function handleResponse(response) {
         console.log(response.data);
